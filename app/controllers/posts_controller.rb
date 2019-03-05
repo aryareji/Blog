57988431e90a88a1230b139aba06de5e4114b7f9
Arya Reji <aryareji119@gmail.com>
2019-03-05 11:02:31 +0530

    Post validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
-	def new; end
-	 def index
+  def new
+     @post = Post.new
+  end
+
+  def index
     @posts = Post.all
   end
  
@@ -8,21 +11,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-	def show
-    @post = Post.find(params[:id])
-  end
-
   def create
-    @post = Post.new(post_params)
-    @post.save
+  @post = Post.new(post_params)
+ 
+  if @post.save
     redirect_to @post
+  else
+    render 'new'
+  end
+end
+ 
+def update
+  @post = Post.find(params[:id])
+ 
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
 end
  
 private
-
   def post_params
     params.require(:post).permit(:title, :text)
   end
- 
-	
 end
