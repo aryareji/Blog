ead7e30272a08969e4cca144b84de8a94a461ae4
Arya Reji <aryareji119@gmail.com>
2019-03-05 12:46:38 +0530

    Delete post records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,8 +34,13 @@ def update
     render 'edit'
   end
   end
- 
-private
+
+  def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
+  end
+  private
   def post_params
     params.require(:post).permit(:title, :text)
   end
