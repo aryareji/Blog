106aa9d5fb465962e21b1d89e7855cf1f36f33a1
Arya Reji <aryareji119@gmail.com>
2019-03-05 22:02:43 +0530

    Alignment fixed in createfeature file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,19 @@
+# createfeature code
 class PostsController < ApplicationController
-def create
-  @post = Post.new(post_params)
- 
-  @post.save
-  redirect_to @post
-end
- 
-private
-  def post_params
-    params.require(:post).permit(:title, :text)
+  def new end;
+
+  def index end;
+
+  def show end;
+
+  def create
+   @post = Post.new(post_params)
+   @post.save
+   redirect_to @post
   end
 
-	def index
-
-	end
-
-	def show
-
-	end
-	
-	def new
-
-	end
+  private
+   def post_params
+    params.require(:post).permit(:title, :text)
+   end
 end
