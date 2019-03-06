36df4524390c9e5115c00033163a314b6fb556db
Arya Reji <aryareji119@gmail.com>
2019-03-06 11:30:07 +0530

    Aignment corrected in controller file and add updation details

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,41 @@
+# post updation controller file
 class PostsController < ApplicationController
   def new
-     @post = Post.new
+    @post = Post.new
   end
 
   def index
     @posts = Post.all
   end
- 
+
   def show
     @post = Post.find(params[:id])
   end
 
   def edit
-  @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def create
-  @post = Post.new(post_params)
- 
-  if @post.save
-    redirect_to @post
-  else
-    render 'new'
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
- 
-def update
-  @post = Post.find(params[:id])
- 
-  if @post.update(post_params)
-    redirect_to @post
-  else
-    render 'edit'
-  end
-  end
- 
-private
+
+  private
+
   def post_params
     params.require(:post).permit(:title, :text)
   end
